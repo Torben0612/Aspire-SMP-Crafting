a853c86bf844987b39c4a1ecacf35a981528cf06
Torben0612 <24krahnstoevert@brooklinek12.org>
2022-04-15 19:55:57 +0200

    Uploaded current files

---
+++ src/main/java/xyz/aspirenetwork/aspirecrafting/Aspirecrafting.java
@@ -0,0 +1,12 @@
+package xyz.aspirenetwork.aspirecrafting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Aspirecrafting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
